d9c5c402351e51ec18f849b56b7cd757e03aa241
Christopher Lamm <chris@theantichris.com>
2019-12-18 15:38:32 -0500

    outlining an HTML doc

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/theantichris/outline
 
 go 1.13
+
+require golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
+// outline prints the structure of an HTML doc.
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"golang.org/x/net/html"
+)
+
+func main() {
+	rootNode, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "outline: %v\n", err)
+		os.Exit(1)
+	}
+
+	outline(nil, rootNode)
+}
+
+func outline(stack []string, htmlNode *html.Node) {
+	if htmlNode.Type == html.ElementNode {
+		stack = append(stack, htmlNode.Data)
+		fmt.Println(stack)
+	}
+
+	fmt.Println("Entering loop...")
+	for child := htmlNode.FirstChild; child != nil; child = child.NextSibling {
+		outline(stack, child)
+	}
 }
